0c9879c1474903480164656cb9de9b5030e8cef8
ThabangMakhosane <thabangboimakhosane@gmail.com>
2026-06-03 11:32:02 +0200

    Characters

---
+++ PA0404-Apply_Programming.js
@@ -0,0 +1,9 @@
+
+function sum(a,b){
+    let addition = a + b;
+    console.log(addition);
+}
+sum(300,5235);
+let age = 25;
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+console.log(months[0]);
