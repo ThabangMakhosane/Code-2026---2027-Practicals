49b08627aee8d90ac45fce1a6ac5f7ef7b6e95e3
ThabangMakhosane <thabangboimakhosane@gmail.com>
2026-06-03 13:59:49 +0200

    Objects

---
+++ PA0406-Object.js
@@ -0,0 +1,28 @@
+const student1 = {
+    name: "Katlego",
+    surname: "Kimberley",
+    age: 20 ,
+    course: "Software Development",
+    year:1,
+}
+const student2 = {
+    name: "Siyabonga",
+    surname: "Mabaso",
+    age: 21,
+    course: "Software Development",
+    year:2,
+}
+const student3 = {
+    name: "Thandi",
+    surname: "Nkosi",
+    age: 22,
+    course: "Software Development",
+    year:3,
+}
+const student4 = {
+    name: "Lerato",
+    surname: "Peters",
+    age: 23,
+    course: "Software Development",
+    year:4,
+}
