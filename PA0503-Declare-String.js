501641c6e0ed651e070314697cb7621bc9d67a11
ThabangMakhosane <thabangboimakhosane@gmail.com>
2026-06-04 10:17:00 +0200

    Declaring String

---
+++ PA0503-Declare-String.js
@@ -0,0 +1,5 @@
+var student = "Thabo Mangena";
+let age = 25;
+const course = "Software Development";
+let year = 2026;
+console.log("Hello, my name is " + student + ". I am " + age + " years old and I am studying " + course + ".");
