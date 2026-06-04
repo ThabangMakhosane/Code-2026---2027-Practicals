0b9f560a0fb1fc1cc0a994bdd6b94fe1cd573a91
ThabangMakhosane <thabangboimakhosane@gmail.com>
2026-06-04 09:24:28 +0200

    Undifined

---
+++ PA0502-Undifinded.js
@@ -0,0 +1,5 @@
+let name = "Siyabonga";
+var age = 30;
+const course = "Cybersecurity";
+let year = 2025;
+console.log("Hello, my name is " + name + ". I am " + age + " years old and I am studying " + course );
