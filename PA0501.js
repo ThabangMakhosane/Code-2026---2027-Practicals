9ab79bfd814fc342f6697f396f927c34808e546c
Thabang Makhosane <123378801+ThabangMakhosane@users.noreply.github.com>
2026-07-22 15:09:25 +0200

    Add files via upload

---
+++ PA0501.js
@@ -0,0 +1,6 @@
+var student = "Thabo Mangena";
+let age = 25;
+const course = "Software Development";
+console.log("Hello, my name is " + student + ". I am " + age + " years old and I am studying " + course + ".");
+ 
+console.log(age = 18);
